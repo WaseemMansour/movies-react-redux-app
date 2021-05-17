8fdd4437e0b5b70ecbd2718c567f8c97729f9605
Waseem Mansour <waseem.mn2002@gmail.com>
2021-05-17 13:22:53 +0200

    Exclude Logger Middleware in Test Mode

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore } from 'redux';
 import reducer from './reducers';
-import middleware from './middleware'
+import { middleware, middlewareWithLogger } from './middleware'
 
-const store = createStore(reducer, middleware);
+const store = createStore(reducer, process.env.NODE_ENV === 'development' ? middlewareWithLogger : middleware );
 export default store;
